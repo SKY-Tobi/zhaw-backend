1e4daf7ffdb1dac1930ed6ebb9413896ffdcffa1
Tobias Heierli <heiertob@students.zhaw.ch>
2023-09-21 10:38:49 +0200

    Project Initialization

---
+++ src/main/java/ch/zhaw/heto/zhaw/ZhawBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.heto.zhaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhawBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhawBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/zhaw/heto/zhaw/ZhawBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.heto.zhaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhawBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
